453aea0613563113b52f4b86b805eaf3dd37e850
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-27 22:14:35 +0800

    okay that didn't work, trying another approach.

--- writer.py
+++ writer.py
@@ -1,36 +1,39 @@
 #grab the text file to write on.
 
-with open("mystory.txt", 'w') as my_file:
-    #create a UI
-    print("Welcome to the typewriting system!")
 
-    #start a loop, make a while condition.
+#create a UI
+print("Welcome to the typewriting system!")
 
-    user_stop = False
+#start a loop, make a while condition.
 
-    while user_stop == False:
-        #call the input.
-        print("Selection: \n(1) Write a new line\n(2) Read the current list\n(3) close the program")
-        selection = int(input("Please type the number of your selection: "))
+user_stop = False
 
-        #if decision is 3, stop the program:
-        if selection == 3:
-            print("Closing the program, goodbye!")
-            break
+while user_stop == False:
+    #call the input.
+    print("Selection: \n(1) Write a new line\n(2) Read the current list\n(3) close the program")
+    selection = int(input("Please type the number of your selection: "))
 
-        #if selection is 1:
-        if selection == 1:
-            #start a while loop for the editor:
-            more_edit = True
-            while more_edit == True:
-                writer = str(input("Please type in a new sentence: "))
-                #write on the lines
-                my_file.write(writer + '\n')
-                #ask if they wish to write again
-                new_edit = str(input("Do you wish to add another line? (Y/N): "))
-                #add conditions:
-                if new_edit.lower() == "n":
-                    print("Returning to main menu.")
-                    more_edit = False
+    #if decision is 3, stop the program:
+    if selection == 3:
+        print("Closing the program, goodbye!")
+        break
+
+    #if selection is 1:
+    if selection == 1:
+        #start a while loop for the editor:
+        my_file = open("mystory.txt", 'r')
+        #count the number of lines
+        line_count = 0
+        for line in my_file:
+            line_count = line_count + 1
+        my_file.close()
+        #if no lines found, write a new line.
+        if line_count == 0:
+
+            first_line = str(input("Type in your first line: "))
+            #write down first line then close.
+            my_file = open("mystory.txt", 'w')
+            my_file.write(first_line + '\n')
+            my_file.close()
 
 
