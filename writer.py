449f5bdef899708f8f6b4ebf829a1f059f5fbab8
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-27 21:14:17 +0800

    adding the selection screen

--- writer.py
+++ writer.py
@@ -3,4 +3,12 @@
 with open("mystory.txt") as my_file:
     #create a UI
     print("Welcome to the typewriting system!")
-    
+
+    #start a loop, make a while condition.
+
+    user_stop = False
+
+    while user_stop == False:
+        #call the input.
+        print("Selection: \n(1) Write a new line\n(2) Read the current list\n(3) close the program")
+        selection = int(input("Please type the number of your selection: "))
