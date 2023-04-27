ad77f77a7244789971dc3fd9751260f12dffd776
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-27 21:19:24 +0800

    added ability to close program and start editing

--- writer.py
+++ writer.py
@@ -12,3 +12,17 @@ with open("mystory.txt") as my_file:
         #call the input.
         print("Selection: \n(1) Write a new line\n(2) Read the current list\n(3) close the program")
         selection = int(input("Please type the number of your selection: "))
+
+        #if decision is 3, stop the program:
+        if selection == 3:
+            print("Closing the program, goodbye!")
+            break
+
+        #if selection is 1:
+        if selection == 1:
+            #start a while loop for the editor:
+            more_edit = True
+            while more_edit == True:
+                writer = str(input("Please type in a new sentence: "))
+
+
